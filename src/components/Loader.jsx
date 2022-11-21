9d5e23ba976913f6101e4788ee69793e504291d5
Praveen K M <99317905+PKM777@users.noreply.github.com>
2022-11-21 21:19:39 +0530

    set loading svg

---
+++ src/components/Loader.jsx
@@ -0,0 +1,13 @@
+import loader from "../assets/loader.svg";
+
+const Loader = () => {
+  return (
+    <div className="bg-black bg-opacity-50 flex items-center justify-center fixed left-0 right-0 bottom-0 top-0 z-50">
+      <div>
+        <img src={loader} alt="loading" className="h-24" />
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
--- src/components/Out.jsx
+++ src/components/Out.jsx
@@ -1,10 +1,11 @@
 import { Outlet, Navigate } from "react-router-dom";
 import { useLoginCheck } from "../hooks/useLoginCheck";
+import Loader from "./Loader";
 
 const Out = () => {
   const { login, status } = useLoginCheck();
   if (status) {
-    return <h3>Loading..</h3>;
+    return <Loader />;
   }
   return login ? <Outlet /> : <Navigate to="/sign-in" />;
 };
